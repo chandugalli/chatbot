4846937f3479201e4647a925fc55626603088f46
Chandu kumar <chandukumar@Chandus-MacBook-Air.local>
2026-04-02 17:01:27 +0530

    fixed login_page error

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login_page, name="login"),
+    path("", views.login_view, name="login"),   # ✅ FIXED
     path("register/", views.register_page, name="register"),
     path("search/", views.search_page, name="search"),
-    path("logout/", views.logout_page, name="logout"),
+    path("logout/", views.logout_view, name="logout"),
 ]
